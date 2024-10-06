9d128a821f019b8b5c02352aa52e537f3e2c68c1
Jared Hane <JaredHane98@gmail.com>
2024-10-06 11:49:13 -0400

    Delete redis-operator/deploy.sh

--- redis-operator/deploy.sh
+++
@@ -1,4 +0,0 @@
-docker build -t redis-operator/container .
-docker tag redis-operator/container:latest public.ecr.aws/f1r9h5l7/redis-operator/container:latest
-docker push public.ecr.aws/f1r9h5l7/redis-operator/container:latest
-#make deploy IMG=public.ecr.aws/f1r9h5l7/redis-operator/container:latest
